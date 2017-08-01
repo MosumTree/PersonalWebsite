5fc52d6fee2c092885d814e352a47bc608dc2d30
Mosumtree <mosumtree@gmail.com>
2017-08-01 23:31:04 +0800

    add goals of articalView

--- view/artical/artical.js
+++ view/artical/artical.js
@@ -6,7 +6,17 @@ import mdStyle from '../../resources/css/markdown.css'
 import {markdown} from 'markdown'
 import html from './articalFile/array.md'
 export default React.createClass({
-    
+    /* 本页面要实现的目标：
+    左侧侧边栏内容；
+    顶部栏需不需要，和侧边栏放在一起会不会拥挤
+    返回首页操作；
+    回到顶部操作，页面目录；
+    文章列表和打开前的预览；
+    md文档从数据库读取；
+    建立从前端显示到后端接口与数据的连接；
+    留言或者说评论功能；
+    写文章的日期显示；
+     */
     render(){
         let articalContext = html;
         var a = require('./articalFile/array.md');
