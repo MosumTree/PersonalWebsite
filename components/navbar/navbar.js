f3bdbe904d36b8ffc879f95866ca9692b685236d
mosum <mosumtree@gamil.com>
2017-08-04 18:55:40 +0800

    add artical view component

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,30 +1,16 @@
 import React,{Component} from 'react';
 import Style from './navbar.less';
 import PropTypes from 'prop-types';
-import Drawer from 'material-ui/Drawer';
-import MenuItem from 'material-ui/MenuItem';
-import RaisedButton from 'material-ui/RaisedButton';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-import {List, ListItem} from 'material-ui/List';
-import ContentInbox from 'material-ui/svg-icons/content/inbox';
-import ActionGrade from 'material-ui/svg-icons/action/grade';
-import ContentSend from 'material-ui/svg-icons/content/send';
-import ContentDrafts from 'material-ui/svg-icons/content/drafts';
-import Divider from 'material-ui/Divider';
-import ActionInfo from 'material-ui/svg-icons/action/info';
-injectTapEventPlugin();
 class Navbar extends Component{
     constructor(props){
         super(props);
         this.state = {
-            isShow: true,
-            open: false
+            isShow: true
         }
     }
     backToIndex(){
         this.props.history.push('/');
     }
-    handleToggle = () => this.setState({open: !this.state.open});
     render(){
         let _this = this;
         let isShow = _this.state.isShow;
@@ -32,30 +18,10 @@ class Navbar extends Component{
         return isShow?
             <div>
                 <div className = {Style.header}>
-                    <i className ={Style.back} onClick = {_this.handleToggle}></i>
+                    <i className ={Style.back}></i>
                     <p className ={Style.title}>{title}</p>
                     <i className ={Style.setting}></i>
                 </div>
-                <Drawer open={this.state.open} width={200} containerClassName={Style.drawer}>
-                        <List>
-                            <ListItem primaryText="Inbox" leftIcon={<ContentInbox />} />
-                            <ListItem primaryText="Starred" leftIcon={<ActionGrade />} />
-                            <ListItem primaryText="Sent mail" leftIcon={<ContentSend />} />
-                            <ListItem primaryText="Drafts" leftIcon={<ContentDrafts />} />
-                            <ListItem primaryText="Inbox" leftIcon={<ContentInbox />} />
-                        </List>
-                        <Divider />
-                        <List>
-                            <ListItem primaryText="All mail" rightIcon={<ActionInfo />} />
-                            <ListItem primaryText="Trash" rightIcon={<ActionInfo />} />
-                            <ListItem primaryText="Spam" rightIcon={<ActionInfo />} />
-                            <ListItem primaryText="Follow up" rightIcon={<ActionInfo />} />
-                            <ListItem primaryText="All mail" rightIcon={<ActionInfo />} />
-                            <ListItem primaryText="Trash" rightIcon={<ActionInfo />} />
-                            <ListItem primaryText="Spam" rightIcon={<ActionInfo />} />
-                            <ListItem primaryText="Follow up" rightIcon={<ActionInfo />} />
-                        </List>
-                </Drawer>
             </div>:null;
     }
 }
--- view/artical/artical.js
+++ view/artical/artical.js
@@ -7,8 +7,26 @@ import {markdown} from 'markdown'
 import IconButton from 'material-ui/IconButton';
 import ActionGrade from 'material-ui/svg-icons/Editor/vertical-align-top';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
-import html from './articalFile/array.md'
+import html from './articalFile/array.md';
+import Swipeable from 'react-swipeable';
+import {List, ListItem} from 'material-ui/List';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import ContentInbox from 'material-ui/svg-icons/content/inbox';
+import ContentSend from 'material-ui/svg-icons/content/send';
+import ContentDrafts from 'material-ui/svg-icons/content/drafts';
+import Divider from 'material-ui/Divider';
+import ActionInfo from 'material-ui/svg-icons/action/info';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import ArticalView from '../../components/articalView/articalView'
+injectTapEventPlugin();
 export default React.createClass({
+    //
+    getInitialState: function() {
+        return {
+            showDrawer: false
+        };
+    },
     /* 本页面要实现的目标：
     左侧侧边栏内容；
     顶部栏需不需要，和侧边栏放在一起会不会拥挤
@@ -20,15 +38,35 @@ export default React.createClass({
     留言或者说评论功能；
     写文章的日期显示；
      */
+    swipingLeft(e, absX) {
+            this.setState({
+                showDrawer:false
+            });
+    },
+    swiping(e, deltaX, deltaY, absX, absY, velocity) {
+        if (deltaX<0&&velocity>1&&Math.abs(deltaY)<10) {
+            this.setState({
+                showDrawer:true
+            });
+        }
+        else if (deltaX>=0) {
+            this.setState({
+                showDrawer:false
+            });
+        }
+    },
     goToTop(e){
         document.documentElement.scrollTop = document.body.scrollTop =0;
     },
     render(){
         let articalContext = html;
         var a = require('./articalFile/array.md');
+        let isShowDrawer = this.state.showDrawer;
         return <MuiThemeProvider>
+            <Swipeable onSwiping={this.swiping}>
             <div>
-                <Navbar title = {'文章'}/>
+                {/*<Navbar title = {'文章'}/>*/}
+                <ArticalView/>
                 <div id="container"  className={mdStyle.markdownBody} dangerouslySetInnerHTML={{__html: a}}> 
                     {/*{articalContext}*/}
                 </div>
@@ -37,7 +75,28 @@ export default React.createClass({
                     <ActionGrade color="red"/>
                     </FloatingActionButton>
                 </div>
+                <Drawer open={isShowDrawer} width={200}>
+                        <List>
+                            <ListItem primaryText="Inbox" leftIcon={<ContentInbox />} />
+                            <ListItem primaryText="Starred" leftIcon={<ActionGrade />} />
+                            <ListItem primaryText="Sent mail" leftIcon={<ContentSend />} />
+                            <ListItem primaryText="Drafts" leftIcon={<ContentDrafts />} />
+                            <ListItem primaryText="Inbox" leftIcon={<ContentInbox />} />
+                        </List>
+                        <Divider />
+                        <List>
+                            <ListItem primaryText="All mail" rightIcon={<ActionInfo />} />
+                            <ListItem primaryText="Trash" rightIcon={<ActionInfo />} />
+                            <ListItem primaryText="Spam" rightIcon={<ActionInfo />} />
+                            <ListItem primaryText="Follow up" rightIcon={<ActionInfo />} />
+                            <ListItem primaryText="All mail" rightIcon={<ActionInfo />} />
+                            <ListItem primaryText="Trash" rightIcon={<ActionInfo />} />
+                            <ListItem primaryText="Spam" rightIcon={<ActionInfo />} />
+                            <ListItem primaryText="Follow up" rightIcon={<ActionInfo />} />
+                        </List>
+                </Drawer>
             </div>
+            </Swipeable>
         </MuiThemeProvider>
     }
 })
